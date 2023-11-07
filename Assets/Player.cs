4dfd3f0d6f358dedf5ea68499defd0758103743f
rapphyboi <deathlydeathdead@gmail.com>
2023-11-08 01:53:02 +0800

    Added Movement

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -2,15 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using static UnityEditor.Timeline.TimelinePlaybackControls;
 
 public class Player : MonoBehaviour
 {
-    public void UseSkill(InputAction.CallbackContext context)
-    {
-        if(context.phase == InputActionPhase.Performed)
-        {
-            Debug.Log("Use Skill");
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private float moveSpeed = 1f;
+    private PlayerInput playerInput;
+    private PlayerInputActions playerInputActions;
 
+    private void Awake()
+    {
+        playerInputActions = new PlayerInputActions();
+        playerInputActions.Player.Enable();
+        playerInputActions.Player.UseSkill.performed += UseSkill_performed;
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 inputVector = playerInputActions.Player.Movement.ReadValue<Vector2>();
+        Debug.Log(inputVector);
+        rb.velocity = inputVector * moveSpeed;
+    }
+
+
+    private void UseSkill_performed(InputAction.CallbackContext context)
+    {
+        Debug.Log(context);
+        if (context.phase == InputActionPhase.Performed)
+        {
+            Debug.Log("Used Skill");
         }
     }
+
+    /*public void UseSkill(InputAction.CallbackContext context)
+    {
+        
+    }*/
 }
