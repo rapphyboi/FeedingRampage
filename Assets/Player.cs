d5b0a9e969eb06afa86b7d49ba1d0126c2df2b23
rapphyboi <deathlydeathdead@gmail.com>
2023-11-09 16:36:53 +0800

    Added Movement and Scaling

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -6,8 +6,11 @@ using static UnityEditor.Timeline.TimelinePlaybackControls;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] private float scaleIndex = 1f;
+
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private float moveSpeed = 1f;
+    [SerializeField] private Animator animator;
     private PlayerInput playerInput;
     private PlayerInputActions playerInputActions;
 
@@ -16,6 +19,24 @@ public class Player : MonoBehaviour
         playerInputActions = new PlayerInputActions();
         playerInputActions.Player.Enable();
         playerInputActions.Player.UseSkill.performed += UseSkill_performed;
+        playerInputActions.Player.Movement.performed += Movement_performed;
+        playerInputActions.Player.Movement.canceled += Movement_canceled;
+    }
+
+    private void Movement_canceled(InputAction.CallbackContext obj)
+    {
+        if (obj.canceled)
+        {
+            animator.SetBool("IsSwimming", false);
+        }
+    }
+
+    private void Movement_performed(InputAction.CallbackContext obj)
+    {
+        if (obj.performed)
+        {
+            animator.SetBool("IsSwimming", true);
+        }
     }
 
     private void FixedUpdate()
@@ -23,6 +44,7 @@ public class Player : MonoBehaviour
         Vector2 inputVector = playerInputActions.Player.Movement.ReadValue<Vector2>();
         Debug.Log(inputVector);
         rb.velocity = inputVector * moveSpeed;
+        
     }
 
 
@@ -32,6 +54,15 @@ public class Player : MonoBehaviour
         if (context.phase == InputActionPhase.Performed)
         {
             Debug.Log("Used Skill");
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<Food>())
+        {
+            Vector3 foodSize = collision.transform.localScale;
+            transform.localScale += foodSize * scaleIndex;
         }
     }
 
